07f0264bb8c5254604cf975160dedf49bd4e5e9e
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-03-27 20:15:45 -0700

    Remove confusion between install_common_scripts.sh (deleted) and perform_common_setup.sh

--- docker_setup_scripts/perform_common_setup.sh
+++ docker_setup_scripts/perform_common_setup.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# Setup shared between multiple systems
+
 set -euo pipefail
 
-adduser --disabled-password --gecos "" yugabyteci
+if [[ $OSTYPE == linux* ]]; then
+  adduser --disabled-password --gecos "" yugabyteci
+fi
 
 cd "${BASH_SOURCE[0]%/*}"
 
-./install_hub_tool.sh
-./install_ninja.sh
-./install_shellcheck.sh
+# These scripts might not executable once copied into the Docker container.
+bash ./install_hub_tool.sh
+bash ./install_ninja.sh
+bash ./install_shellcheck.sh
