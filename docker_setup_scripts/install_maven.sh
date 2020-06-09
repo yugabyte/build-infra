0132c3f3e3e07de17e086d27804c597f49508cd6
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-06-08 17:20:01 -0700

    Fix Maven installation script

--- docker_setup_scripts/install_maven.sh
+++ docker_setup_scripts/install_maven.sh
@@ -2,18 +2,37 @@
 
 set -euo pipefail -x
 
-version=3.6.3
-maven_dir_name=apache-maven-$version
-tarball_name=$maven_dir_name-bin.tar.gz
-url="https://apache.osuosl.org/maven/maven-3/$version/binaries/$tarball_name"
-cd /tmp
-curl -O "$url"
-cd /usr/share
-tar xzf "/tmp/$tarball_name"
-mkdir -p /usr/local/bin
-mvn_binary_path="/usr/share/$maven_dir_name/bin/mvn"
-if [[ ! -e $mvn_binary_path ]]; then
-  echo "$mvn_binary_path does not exist" >&2
-  exit 1
-fi
-ln -s "$mvn_binary_path" /usr/local/bin
+install_maven() {
+  local version=3.6.3
+  local maven_dir_name=apache-maven-$version
+  local tarball_name=$maven_dir_name-bin.tar.gz
+  local url="https://apache.osuosl.org/maven/maven-3/$version/binaries/$tarball_name"
+  local maven_tmp_dir=/tmp/install_maven
+  local dest_dir=/usr/share/$maven_dir_name
+  local mvn_link_path=/usr/local/bin/mvn
+  if [[ -d $dest_dir &&
+        $(readlink "$mvn_link_path") == "$dest_dir/bin/mvn" ]]; then
+    echo "Maven is already installed at $dest_dir and symlinked to /usr/local/bin"
+    return
+  fi
+  mkdir -p "$maven_tmp_dir"
+  (
+    cd "$maven_tmp_dir"
+    rm -rf "$maven_tmp_dir/"*
+    curl -O "$url"
+    tar xzf "$tarball_name"
+  )
+  sudo rm -rf "$dest_dir"
+  sudo mv "$maven_tmp_dir/$maven_dir_name" "$dest_dir"
+  sudo rm -rf "$maven_tmp_dir"
+  sudo mkdir -p /usr/local/bin
+  local mvn_binary_path="/usr/share/$maven_dir_name/bin/mvn"
+  if [[ ! -e $mvn_binary_path ]]; then
+    echo "$mvn_binary_path does not exist" >&2
+    exit 1
+  fi
+  sudo rm -f "$mvn_link_path"
+  sudo ln -s "$mvn_binary_path" "$mvn_link_path"
+}
+
+install_maven
