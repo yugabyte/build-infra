0ba043571a95e72911f66255e76154baeb83a4af
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-11-07 13:30:02 -0600

    Install LLVM 11 in the CentOS 7 image

--- docker_setup_scripts/create_opt_yb-build_dirs.sh
+++ docker_setup_scripts/create_opt_yb-build_dirs.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-for dir_name in brew download_cache thirdparty tmp; do
+for dir_name in brew download_cache thirdparty tmp llvm; do
   dir_path=/opt/yb-build/$dir_name
-  ( 
+  (
     set -x
     mkdir -p "$dir_path"
     chmod 777 "$dir_path"
