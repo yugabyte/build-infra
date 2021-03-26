5ae890ed3be1dfc21b43e22d9e5d825a5e460ee8
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-03-25 22:02:14 -0700

    Support Ubuntu 20.04 (#28)

--- docker_setup_scripts/ubuntu.sh
+++ docker_setup_scripts/ubuntu.sh
@@ -7,6 +7,9 @@ apt-get update
 apt-get dist-upgrade -y
 echo "::endgroup::"
 
+. /etc/lsb-release
+ubuntu_major_version=${DISTRIB_RELEASE%%.*}
+
 packages=(
     apt-file
     apt-utils
@@ -16,39 +19,59 @@ packages=(
     flex
     git
     golang
+    groff-base
     less
+    libasan5
     libbz2-dev
     libicu-dev
     libncurses5-dev
     libreadline-dev
     libssl-dev
     libtool
+    libtsan0
     locales
     maven
     openjdk-8-jdk-headless
     pkg-config
-    python-dev
-    python-pip
     python3-dev
     python3-pip
     python3-venv
     python3-wheel
     rsync
     sudo
+    tzdata
     unzip
     uuid-dev
     vim
     wget
     xz-utils
-    groff-base
-    gcc-8
-    g++-8
-    libasan5
-    libtsan0
 )
 
+if [[ $ubuntu_major_version -le 18 ]]; then
+  packages+=(
+    python-pip
+    python-dev
+    gcc-8
+    g++-8
+  )
+fi
+
+export LANG=en_US.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+
 echo "::group::Installing Ubuntu packages"
-apt-get install -y "${packages[@]}"
+for package in "${packages[@]}"; do
+  echo "------------------------------------------------------------------------------------------"
+  echo "Installing package $package and its dependencies"
+  echo "------------------------------------------------------------------------------------------"
+  echo
+  apt-get install -y "$package"
+  echo
+  echo "------------------------------------------------------------------------------------------"
+  echo "Finished installing package $package and its dependencies"
+  echo "------------------------------------------------------------------------------------------"
+  echo
+done
 echo "::endgroup::"
 
 echo "::group::Installing LLVM/Clang packages"
