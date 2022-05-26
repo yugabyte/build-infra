362a1f2415496a9f7f05c672a507c8e1cdd7b6d9
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2022-05-26 00:38:32 -0700

    [skip ci] Do not hard-code architecture in install_shellcheck.sh (to fix aarch64)

--- docker_setup_scripts/install_shellcheck.sh
+++ docker_setup_scripts/install_shellcheck.sh
@@ -5,6 +5,6 @@ set -euo pipefail -x
 # Instructions from https://github.com/koalaman/shellcheck
 
 scversion="stable" # or "v0.4.7", or "latest"
-wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.$( uname -m ).tar.xz" | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 shellcheck --version
