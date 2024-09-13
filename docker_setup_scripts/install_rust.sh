db990a166f3cb6e132d7235ed69cca4e2c9b0da1
saratkumar-yb <skarri@yugabyte.com>
2024-09-14 00:05:25 +0530

    [DEVOPS-3191] Install Rust

--- docker_setup_scripts/install_rust.sh
+++ docker_setup_scripts/install_rust.sh
@@ -8,7 +8,7 @@ install_dependencies() {
   if [ -f /etc/os-release ]; then
     . /etc/os-release
     case "$ID" in
-      amzn|rhel|centos|fedora)
+      amzn|rhel|centos|almalinux|fedora)
         yum install -y curl gcc && yum clean all
         ;;
       ubuntu|debian)
